32b64544c43ad7c9129952e36c7de5f04b3aa48c
LuisGustavoOB <luisgustavo.ob@hotmail.com>
2024-07-31 15:33:26 -0300

    js

--- aulas-js/js/index.js
+++ aulas-js/js/index.js
@@ -2,3 +2,6 @@
 document.getElementById("texto").innerHTML="Meu primeiro texto  <b>JS</b>!";
 alert('Mais uma maneira de saida de dados pelo JS'); // são maneiras de saida de dados do JS
 console.log("Oi isso é um alerta, mais uma forma de saida de dados pelo JS, de forma mais escondida.");
+
+
+
