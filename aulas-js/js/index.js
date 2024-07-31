6c6c38ff1b6e07000bc465d0538099b41357d8cf
LuisGustavoOB <luisgustavo.ob@hotmail.com>
2024-07-31 15:46:15 -0300

    js

--- aulas-js/js/index.js
+++ aulas-js/js/index.js
@@ -3,5 +3,11 @@ document.getElementById("texto").innerHTML="Meu primeiro texto  <b>JS</b>!";
 alert('Mais uma maneira de saida de dados pelo JS'); // são maneiras de saida de dados do JS
 console.log("Oi isso é um alerta, mais uma forma de saida de dados pelo JS, de forma mais escondida.");
 
+var a = 1;
+var b = 5;
+var c = a + b;
+alert(c);
+console.log(c);
 
+// o JavaScript ele muda conforme tamanho das letras, então tem que escrever exato as variaveis e nomes. 
 
