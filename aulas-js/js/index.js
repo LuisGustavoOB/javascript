ea04b367905d4151f0507042622d1fef32f656b5
LuisGustavoOB <luisgustavo.ob@hotmail.com>
2024-07-31 15:14:28 -0300

    JS

---
+++ aulas-js/js/index.js
@@ -0,0 +1,4 @@
+//Arquivo externo, de um index direto de Java Script
+document.getElementById("texto").innerHTML="Meu primeiro texto  <b>JS</b>!";
+alert('Mais uma maneira de saida de dados pelo JS'); // são maneiras de saida de dados do JS
+console.log("Oi isso é um alerta, mais uma forma de saida de dados pelo JS, de forma mais escondida.");
